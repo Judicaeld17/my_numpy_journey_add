619badfef0a8e5ad4d195361dfcf4cde273ff464
Judicaeld17e <judicaeldjidonou3@gmail.com>
2023-11-25 18:39:26 +0100

    first commit

---
+++ my_numpy_journey_add.py
@@ -0,0 +1,4 @@
+#TASK : Create a function which add two numpy array. It will receive two numpy array and return the result of the operation.
+import numpy as np
+def my_numpy_journey_add(np_array1,np_array2):
+    return  np.add(np_array1,np_array2)
